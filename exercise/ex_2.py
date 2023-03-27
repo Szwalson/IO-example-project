1f3163d48aacfc45e13f48791576fb2a201878cd
Wojtek <layvin@interia.pl>
2023-03-27 12:17:10 +0200

    Dodanie prostokąta

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,8 +1,14 @@
 # kwadrat
+a = 10
+obwod1 = a * 4
+pole1 = a ** 2
+print('Obwód kwadratu wynosi ' + str(obwod1) + ', a pole wynosi ' + str(pole1) + '.')
+
+# prostokąt
 
 a = 10
+b = 5
 
-obwod = a * 4
-pole = a ** 2
-
-print('Obwód kwadratu wynosi ' + str(obwod) + ', a pole wynosi ' + str(pole) + '.')
+obwod2 = 2*a+2*b
+pole2 = a*b
+print('Obwód prostokąta wynosi ' + str(obwod2) + ', a pole wynosi ' + str(pole2) + '.')
