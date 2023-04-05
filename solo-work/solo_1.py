12a51ecd11ff84f7cf056d4cd23977113d300341
Wojtek <layvin@interia.pl>
2023-04-05 11:06:22 +0200

    Zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -14,3 +14,13 @@ print("{message} {name}".format(message=hello, name=student))
 student = input("Wpisz swoje imię: ")
 
 print("Hello {name}".format(name=student))
+
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi: {number}".format(number=liczba_studentow))
