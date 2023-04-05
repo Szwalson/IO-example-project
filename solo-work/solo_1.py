9ac643bc51352ed25332ae38fa99643f19e477a8
Wojtek <layvin@interia.pl>
2023-04-05 10:50:07 +0200

    Zadanie 1.1

---
+++ solo-work/solo_1.py
@@ -0,0 +1,9 @@
+# zadanie 1.1
+
+hello = "Hello"
+student = "Wojtek"
+
+# oczekiwany rezultat: Hello Wojtek
+# wykorzystaj w princie zmienne hello i student
+
+print("{message} {name}".format(message=hello, name=student))
