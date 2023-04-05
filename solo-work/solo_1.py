238c40198ebdd8c42ae9c3e7ec2d3c1822999dd6
Wojtek <layvin@interia.pl>
2023-04-05 12:45:28 +0200

    Sprzatanie

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,35 +1,28 @@
-# zadanie 1.1
+import math
+import numpy
 
-hello = "Hello"
-student = "Wojtek"
-
+# Zadanie 1.1
 # oczekiwany rezultat: Hello Wojtek
 # wykorzystaj w princie zmienne hello i student
 
+hello = "Hello"
+student = "Wojtek"
 print("{message} {name}".format(message=hello, name=student))
 
-
-# zadanie 1.2
+# Zadanie 1.2
 
 student = input("Wpisz swoje imie: ")
-
 print("Hello {name}".format(name=student))
 
-
-# zadanie 1.3
-
-studenci = ["Ania", "Kuba", "Piotr", "Jan"]
-
+# Zadanie 1.3
 # policz liczbe studentow w tablicy studenci
 # oczekiwany rezultat: Liczba studentow wynosi: 4
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
 liczba_studentow = len(studenci)
 print("Liczba studentow wynosi: {number}".format(number=liczba_studentow))
 
-
-# zadanie 1.4
-
-studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
-
+# Zadanie 1.4
 # za pomoca petli i print przywitaj sie z kazdym studentem
 # z tabeli studenci osobno
 # oczekiwany rezultat:
@@ -37,109 +30,99 @@ studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
 # Hello Kasia
 # Hello Piotr
 # Hello Tomek
+
+studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
 for i in studenci:
     print("Hello {name}".format(name=i))
 
-
-# zadanie 1.5
-
-import math
+# Zadanie 1.5
+# oczekiwany rezultat:
+# Wynik wynosi: 81
 
 liczba = 3
 potega = 4
-
 wynik = math.pow(liczba, potega)
+print("Wynik wynosi: " + str(wynik))
 
-# oczekiwany rezultat:
-# Wynik wynosi: 81
-print("Wynik wynosi: "+str(wynik))
-
-
-# zadanie 1.6
-
+# Zadanie 1.6
 # policz ilosc nawiasow ( w danym ciagu znakow
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
 
 ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
 liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
-
-# oczekiwany rezultat:
-# Liczba nawiasow otwierajacych wynosi: 4
 print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
 
-
-# zadanie 1.7
-
-import numpy
-
+# Zadanie 1.7
 # posortuj alfabetycznie (od imienia) studentow
-studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
-
 # oczekiwany rezultat:
 # Anna Szczesny
 # Barbara Kowalska
 # Jan Niezbedny
 # Tomasz Nijaki
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 print("Alfabetyczna lista studentow wynosi: ")
 for student in numpy.sort(studenci):
     print(student)
 
-
-# zadanie 1.8
-
+# Zadanie 1.8
 # posortuj alfabetycznie (od nazwiska) studentow
+# oczekiwany rezultat:
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+# Anna Szczesny
+
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
 
 def sortowanie(lista_wejsciowa):
     lista_wejsciowa.sort(key=lambda x: x.split()[-1])
     return lista_wejsciowa
 
-# oczekiwany rezultat:
-# Barbara Kowalska
-# Jan Niezbedny
-# Tomasz Nijaki
-# Anna Szczesny
+
 print("Alfabetyczna lista studentow wynosi: ")
 for student in sortowanie(studenci):
     print(student)
 
-
-# zadanie 1.9
-
+# Zadanie 1.9
 # policz wszystkich studentow z tablicy, ktorych nazwisko zaczyna sie na N
+
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 studenci_lastnames = []
 for student in studenci:
     studenci_lastnames.append(student.split()[-1])
-
 studenciN = [x for x in studenci_lastnames if x.startswith("N")]
-
 liczba_n = len(studenciN)
-print("Liczba studentow na N wynosi: "+str(liczba_n))
+print("Liczba studentow na N wynosi: " + str(liczba_n))
 
-
-# zadanie 1.10
-
-# zmienne poniezej preprezentuja ulozenie punktow na wykresie,
+# Zadanie 1.10
+# zmienne ponizej preprezentuja ulozenie punktow na wykresie,
 # do zadania dolaczono takze rysunek pomocniczy
-wykres_1 = [[2, 4], [4, 4], [6, 4]]
-wykres_2 = [[2, 3], [4, 4], [6, 5]]
-wykres_3 = [[2, 3], [4, 3], [5, 4]]
-
 # zbadaj kazdy wykres, czy dla wyznaczonych punktow istnieje funkcja
 # liniowa laczaca punkty
 # jesli sie nie da, to zwroc False
 # jesli sie da, zwroc True
+# oczekiwany rezultat:
+# Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.
+# Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.
+# Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.
+
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
 
 def weryfikacja_liniowosci(wykres):
-    a1 = (wykres[1][1]-wykres[0][1])/(wykres[1][0]-wykres[0][0])
-    a2 = (wykres[2][1]-wykres[0][1])/(wykres[2][0]-wykres[0][0])
-
+    a1 = (wykres[1][1] - wykres[0][1]) / (wykres[1][0] - wykres[0][0])
+    a2 = (wykres[2][1] - wykres[0][1]) / (wykres[2][0] - wykres[0][0])
     if a1 == a2:
         wynik = True
     else:
         wynik = False
-
     return wynik
+
 
 wykres_1_funkcja_liniowa = weryfikacja_liniowosci(wykres_1)
 wykres_2_funkcja_liniowa = weryfikacja_liniowosci(wykres_2)
@@ -159,8 +142,3 @@ if wykres_3_funkcja_liniowa:
     print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
 else:
     print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
-
-# oczekiwany rezultat:
-# Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.
-# Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.
-# Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.
