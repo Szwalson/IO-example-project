c331c4f40c253a22fdbab227808b941b3967c301
Wojtek <layvin@interia.pl>
2023-05-22 12:58:09 +0200

    Fibonacci

--- solo-work/solo_recursive.py
+++ solo-work/solo_recursive.py
@@ -12,18 +12,39 @@ def list_sum(lista):
         return el1+list_sum(lista)
 
 lista1 = [3, 2, 3, 4]
-print(list_sum(lista1))
+#print(list_sum(lista1))
 
 # Stworzenie funkcji, która oblicza rekurencyjnie silnię z podanej liczby:
 # funkcja silnia(liczba):
-#   is liczba > 1
-#       yes -> liczba*silnia(liczba-1)
-#       no -> 1
+#   is liczba > 0 ?
+#       no -> błąd
+#       yes -> is liczba > 1?
+#           no -> 1
+#           yes -> liczba * silnia(liczba-1)
+
 
 def silnia(liczba):
     if liczba > 1:
         return (liczba * silnia(liczba-1))
+    elif liczba < 0:
+        return "Błędna wartość wejściowa"
     else:
         return 1
 
-print(silnia(3))
+print(silnia(5))
+
+# Stworzenie funkcji, która zwraca podany element ciągu Fibonacciego
+# funkcja fib(liczba)
+#   is liczba > 2 ?
+#       yes -> fib(liczba-1) + fib(liczba-2)
+#       no -> 1
+
+def fib(liczba):
+    if liczba > 2:
+        return fib(liczba-1) + fib(liczba-2)
+    elif liczba == 0:
+        return 0
+    else:
+        return 1
+
+#print(fib(7))
